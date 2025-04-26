f5c63463e87a657c6d26af7f6d35be8ab2f47383
b0r3d <bogd.reschetko2017@gmail.com>
2025-04-26 11:21:27 +0300

    fixed bugs after merging

--- src/main/com /airline/dto/LoginRequestDTO.java
+++ src/main/com /airline/dto/LoginRequestDTO.java
@@ -1,4 +1,13 @@
 package dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDTO {
+    private String username;
+    private String password;
 }
--- src/main/com /airline/dto/UserDTO.java
+++ src/main/com /airline/dto/UserDTO.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
     private Long id;
     private String username;
+    private String email;
     private String role;
 }
